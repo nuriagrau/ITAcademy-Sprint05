9ed1859fe2cb22bab239072837cb32c0c899d006
nuriagrau <nuriagrau@protonmail.com>
2024-07-19 18:21:48 +0200

    Config files reordered

---
+++ T02/S05T02DiceGame/src/main/java/cat/itacademy/barcelonactiva/grauHorta/nuria/s05/t02/DiceGame/configuration/MapperConfig.java
@@ -0,0 +1,21 @@
+package cat.itacademy.barcelonactiva.grauHorta.nuria.s05.t02.DiceGame.configuration;
+
+import cat.itacademy.barcelonactiva.grauHorta.nuria.s05.t02.DiceGame.model.mapper.GameMapper;
+import cat.itacademy.barcelonactiva.grauHorta.nuria.s05.t02.DiceGame.model.mapper.PlayerMapper;
+import cat.itacademy.barcelonactiva.grauHorta.nuria.s05.t02.DiceGame.model.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public GameMapper GameMapperBean() {
+        return new GameMapper();
+    }
+
+    @Bean
+    public PlayerMapper playerMapperBean() {
+        return new PlayerMapper();
+    }
+}
